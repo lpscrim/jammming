9d08f0151475f26ff8701fec9da3c1d5c64a040e
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2023-05-09 09:16:24 +0100

    Finishing off getAccessToken fucntion

--- src/Utility/Spotify/Spotify.js
+++ src/Utility/Spotify/Spotify.js
@@ -1,10 +1,35 @@
 const clientId = '87cfc90456484b70a7997e4351f62239';
-const redirect_uri = 'http://localhost:8888/callback';
+const encClientId= encodeURIComponent(clientId);
+const redirectUri = 'http://localhost:8888/callback';
+const encRedirect_uri= encodeURIComponent(redirectUri);
 const clientSecret = '7a948a4cc337480c944b750ace086b8e';
+const url = 'https://accounts.spotify.com/authorize?response_type=token&client_id=' + encClientId + '&redirect_uri=' + encRedirect_uri;
 
+let accessToken;
+let expiresIn;
 
+function getAccessToken() {
 
-const url = 'https://accounts.spotify.com/authorize';
-url += '?response_type=token';
-url += '&client_id=' + encodeURIComponent(client_id);
-url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
+    if(accessToken) {
+        return accessToken;    
+    }
+
+    const urlParams = new URLSearchParams(window.location.search);
+    const urlAccessToken = urlParams.get('access_token');
+    const urlExpiresIn = urlParams.get('expires_in');
+
+    if (urlAccessToken && urlExpiresIn) {
+
+        accessToken = urlAccessToken;
+        expiresIn = urlExpiresIn;
+
+        setTimeout(() => {
+            accessToken = '';
+          }, expiresIn * 1000);
+
+        window.history.pushState({}, null, '/');
+
+    } else {
+        window.location = url;
+    }
+};
