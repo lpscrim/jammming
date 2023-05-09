1f61887c072f56585ff5710bf0eb6ae9a9c2ba8a
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2023-05-09 10:22:20 +0100

    added spotify search to Spotify util

--- src/Utility/Spotify/Spotify.js
+++ src/Utility/Spotify/Spotify.js
@@ -33,3 +33,29 @@ function getAccessToken() {
         window.location = url;
     }
 };
+
+async function spotifySearch(term) {
+
+    const accessToken = Spotify.getAccessToken();
+
+    //included limit of 20 tracks and track type in search.
+    const response = await fetch("https://api.spotify.com/v1/search?type=track&limit=20&q=" + term, {
+        headers: { Authorization: "Bearer " + accessToken }
+    });
+
+    const jsonResponse = await response.json();
+
+    if (!jsonResponse.tracks) {
+        return [];
+    };
+
+    return jsonResponse.tracks.items.map((track) => ({
+        id: track.id,
+        name: track.name,
+        artist: track.artists[0].name,
+        album: track.album.name,
+        uri: track.uri
+    }));
+
+}
+
