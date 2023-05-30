d14d4eedd9fa05577eb4c961abaa0cb00f945c27
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2023-05-30 19:34:54 +0100

    addition of playlistlist component

--- src/components/PlaylistList/PlaylistList.js
+++ src/components/PlaylistList/PlaylistList.js
@@ -22,3 +22,5 @@ function PlaylistList(props){
         </div>
     )
 }
+
+export default PlaylistList;
---
+++ src/components/PlaylistListItem/PlaylistListItem.js
@@ -0,0 +1,5 @@
+function PlaylistListItem(props){
+    
+}
+
+export default PlaylistListItem;
